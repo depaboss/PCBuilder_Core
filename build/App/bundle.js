50208826af37d290e01c6539b750e4522a3955c7
Nicola Bellotti <n.bellotti90@gmail.com>
2016-10-26 12:39:31 +0200

    master_init_config

---
+++ build/App/bundle.js
@@ -0,0 +1,48 @@
+(function(){
+  'use strict';
+
+  var App = angular.module('App',[]);
+  
+ })();
+
+(function(){
+  'use strict';
+
+  angular.module('App')
+    .controller('UserController', UserController);
+
+    UserController.$inject = [];
+
+    function UserController(){
+      var vc = this;
+      vc.users=[{
+        nome:'triangolo',
+        cognome: 'triangola',
+        eta: 25,
+        citta: 'triangolandia'
+      },{
+        nome: 'pinco',
+        cognome: 'palla',
+        eta: 22,
+        citta: 'pincopallandia'
+      },{
+        nome: 'birillo',
+        cognome: 'birilla',
+        eta: 62,
+        citta: 'birillandia'
+      }]
+      vc.stile= "border: 2px solid green";
+
+      vc.saluta = function(){
+        console.info(vc.risSaluta);
+        vc.risSaluta = " Ciao " + vc.user.nome +" "+ vc.user.cognome + " di " + vc.user.eta + " anni!! ";
+        console.info(vc.risSaluta);
+
+      };
+
+    };
+  
+
+})();
+
+//# sourceMappingURL=build/App/bundle.js.map
---
+++ server/server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+//CONNESSION DATABASE MONGOLAB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://christian:artoo@ds031167.mlab.com:31167/artoo_be', function(err){
+  if(err){
+    throw err;
+  }
+  console.info("database connesso");
+});
+
+//SETUP DEL SERVER EXPRESS
+var app = express();
+
+//MIDDLEWARE BODY PARSER
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+//FAVICON
+app.use(require('serve-favicon')(path.join(__dirname,"..","client","favicon.ico")));
+
+//SERVE I FILE STATICI
+app.use('/', express.static(path.join(__dirname, '..', 'client')));
+
+//SERVE GLI SCRIPT DENTRO NODE MODULES, PER IL CLIENT
+app.use('/scripts', express.static(path.join(__dirname, '..', 'node_modules')));
+app.use('/bundle', express.static(path.join(__dirname, '..', 'build', 'App')));
+app.use('/vendor', express.static(path.join(__dirname, '..', 'build','vendors')));
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//LE NOSTRE API
+app.use('/heroes',require('./Esemp_Heroes'));
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//SERVE SEMPRE L'INDEX.HTML IN QUALSIASI ALTRO CASO FUORI DALLE API
+app.get('/',function(req, res){
+  res.sendFile(path.join(__dirname,"..","build","index.html"));
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+//START SERVER
+app.listen(3000, function(){
+  console.log('server express start on: http://localhost:' + 3000);
+})
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//LIVERLOAD
+livereload = require('livereload');
+server = livereload.createServer();
+server.watch(path.join(__dirname, '..', 'client', '**', '*'));
